b536788ed0bbc9b1463a9b1428b81c00ea1e7eb4
Nuttida Mueangmoon <nuttm204@mimotechos.com>
2025-03-02 19:19:04 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import HomePage from "./home/page";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Your Custom Website!</h1>
-      <p>This is your custom content.</p>
+      <HomePage />
     </div>
   );
 }
