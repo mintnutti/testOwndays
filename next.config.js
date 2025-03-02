f4861e743f4dd8a7b2fb74e96910d69caac010c8
Nuttida Mueangmoon <nuttm204@mimotechos.com>
2025-03-02 17:31:59 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 module.exports = withBundleAnalyzer({
+  output: "export",
   reactStrictMode: true, // เปิดการใช้งาน React Strict Mode
   compiler: {
     styledComponents: true, // ถ้าคุณใช้ styled-components
