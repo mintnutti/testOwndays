50cbf4706770ead4e2d01fd4c7a2da40497f5354
Nuttida Mueangmoon <nuttm204@mimotechos.com>
2025-03-02 18:12:48 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   output: "export",
+  basePath: "/testOwndays", // ชื่อ repository ของคุณ
+  assetPrefix: "/testOwndays",
   reactStrictMode: true, // เปิดการใช้งาน React Strict Mode
   compiler: {
     styledComponents: true, // ถ้าคุณใช้ styled-components
